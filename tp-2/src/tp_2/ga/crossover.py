a89fd0955d40384155f212e8980ba5611dd175a6
nrivas-itba <138261486+nrivas-itba@users.noreply.github.com>
2026-09-07 04:54:40 -0300

    Unify random and fix edge conditions

--- tp-2/src/tp_2/ga/crossover.py
+++ tp-2/src/tp_2/ga/crossover.py
@@ -1,4 +1,3 @@
-import random
 import numpy as np
 from typing import Tuple
 from tp_2.ga.individual import Individual
@@ -11,7 +10,7 @@ class Crossover:
         n = p1.genome.shape[0]
         if n < 2:
             return Individual(p1.genome.copy()), Individual(p2.genome.copy())
-        point = random.randint(1, n - 1)
+        point = np.random.randint(1, n)
         
         c1_genome = np.concatenate((p1.genome[:point], p2.genome[point:]), axis=0)
         c2_genome = np.concatenate((p2.genome[:point], p1.genome[point:]), axis=0)
@@ -23,7 +22,7 @@ class Crossover:
         n = p1.genome.shape[0]
         if n < 3:
             return Crossover.one_point(p1, p2)
-        pt1, pt2 = sorted(random.sample(range(1, n), 2))
+        pt1, pt2 = np.sort(np.random.choice(np.arange(1, n), size=2, replace=False))
         
         c1 = np.concatenate((p1.genome[:pt1], p2.genome[pt1:pt2], p1.genome[pt2:]), axis=0)
         c2 = np.concatenate((p2.genome[:pt1], p1.genome[pt1:pt2], p2.genome[pt2:]), axis=0)
@@ -33,6 +32,8 @@ class Crossover:
     @staticmethod
     def uniform(p1: Individual, p2: Individual, p: float = 0.5) -> Tuple[Individual, Individual]:
         n = p1.genome.shape[0]
+        if n < 1:
+            return Individual(p1.genome.copy()), Individual(p2.genome.copy())
         mask = (np.random.rand(n) < p).reshape((-1,) + (1,) * (p1.genome.ndim - 1))
         
         c1 = np.where(mask, p1.genome, p2.genome)
@@ -45,8 +46,8 @@ class Crossover:
         n = p1.genome.shape[0]
         if n < 1:
             return Individual(p1.genome.copy()), Individual(p2.genome.copy())
-        start = random.randint(0, n - 1)
-        length = random.randint(1, max(1, n // 2))
+        start = np.random.randint(0, n)
+        length = np.random.randint(1, max(1, n // 2) + 1)
         
         mask = np.zeros((n,) + (1,) * (p1.genome.ndim - 1), dtype=bool)
         for i in range(length):
@@ -65,8 +66,10 @@ class Crossover:
         foreground details based on a non-linear layer-decay function.
         """
         n = p1.genome.shape[0]
-        focal_point = random.random()
-        bandwidth = 0.2 + 0.3 * random.random()
+        if n < 1:
+            return Individual(p1.genome.copy()), Individual(p2.genome.copy())
+        focal_point = np.random.rand()
+        bandwidth = 0.2 + 0.3 * np.random.rand()
 
         layer_ratios = np.arange(n) / max(1, n - 1)
         dists = (layer_ratios - focal_point) / bandwidth
