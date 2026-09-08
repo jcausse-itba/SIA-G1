b71bf6c01cfc87b0bdde14d0642adf87f6010988
nrivas-itba <138261486+nrivas-itba@users.noreply.github.com>
2026-09-08 20:13:14 -0300

    add_triangle_prob

--- tp-2/src/tp_2/ga/engine.py
+++ tp-2/src/tp_2/ga/engine.py
@@ -68,6 +68,7 @@ class GAEngine:
         children_size  = cfg.get("children_size",  pop_size)
         crossover_prob = cfg.get("crossover_prob",  0.8)
         mutation_prob  = cfg.get("mutation_prob",   0.1)
+        add_triangle_prob = cfg.get("add_triangle_prob", 0.01)
         p_tri          = cfg.get("p_tri",           0.10356780876524548)
         p_comp         = cfg.get("p_comp",          0.1107160259183529)
         max_gen        = cfg.get("max_generations", 1000)
@@ -105,6 +106,19 @@ class GAEngine:
 
         while True:
             generation += 1
+            
+            if random.random() < add_triangle_prob:
+                num_triangles += 1
+                for ind in population + [best]:
+                    new_tri = np.array([[
+                        random.random(), random.random(), random.random(),
+                        random.random(), random.random(), random.random(),
+                        random.uniform(0.0, 360.0), random.uniform(0.0, 130.0),
+                        random.uniform(0.0, 100.0), random.uniform(0.05, 1.0)
+                    ]], dtype=np.float32)
+                    ind.genome = np.vstack([ind.genome, new_tri])
+                    ind.fitness = None
+                self.evaluator.evaluate(population + [best])
 
             best_fitness = best.fitness if best.fitness is not None else 0.0
             stop, reason = self.stopping.should_stop(generation, best_fitness)
